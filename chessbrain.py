79b86c9cefdf612c50372085bbd1ba0e3590fe3f
CrabbleOrNiceTry <thebossineldorado@gmail.com>
2021-02-14 12:35:36 -0700

    refactored some stuff

--- chessbrain.py
+++ chessbrain.py
@@ -2,6 +2,7 @@ import chess
 import numpy
 from MoveTree import MoveTree
 
+# This is the way we evaluate the board, If the addition of all piecs on the board with these values is negative black is winning, vise versa
 piece_values = {'P': 10, 'N': 30, 'B': 30, 'R': 50, 'Q': 90, 'K': 100, 'p': -10, 'n': -30, 'b': -30, 'r': -50, 'q': -90, 'k': -100}
 
 
@@ -11,55 +12,114 @@ def main():
 
     while not board.is_game_over():
         print(board)
-        move = input("Move: ")
-        board.push_san(move)
-        ai = MoveTree(board)
-        ai.fillTree()
-        eval, aiMove = minimax(ai, 3, maximizingPlayer=False)
-        board.push(aiMove)
+        while True:
+            try:
+                move = input("Move: ")
+                board.push_san(move)
+            except:
+                continue
+            break
+        val, ai = getMove(board, 3, False)
+        board.push(chess.Move.from_uci(str(ai)))
 
-def boardEval(board, piece_values=piece_values):
-    currEval = 0
+def calculatePos(board, piece_values=piece_values):
     pieces = list(board.piece_map().values())
 
+    eval = 0
+
     for i in pieces:
-        currEval += piece_values[str(i)]
-        print(currEval)
-    return currEval
+        eval += piece_values[str(i)]
+    return eval
 
-def minimax(node, depth, maximizingPlayer = True):
-
-    # Base case
-    print(type(depth))
-    if depth == 0 or node.board.is_game_over():
-        if maximizingPlayer:
-            return boardEval(node.board), node.board
+# simple minimax algorithm, no alpha-beta pruning yet.
+def getMove(board, depth, player):
+    if depth == 0 or board.is_game_over():
+        if player:
+            return int(calculatePos(board))
         else:
-            return -1*boardEval(node.board), node.board
+            return int(-1*calculatePos(board))
 
     # white
-    if maximizingPlayer:
-        value = -numpy.Infinity
-        bestEval = value
-        bestEvalMove = None
-        for i in node.child:
-            value, f = max(value, minimax(i, depth - 1, maximizingPlayer=True))
-            print("as")
-            if value > bestEval:
-                bestEval = value
-                bestEvalMove = f
-        return bestEval, bestEvalMove
-    # black
+    if player:
+        max = -numpy.Infinity
+        move_list = list(board.legal_moves)
+        for i, move in enumerate(move_list):
+
+            # Create a separate board
+            temp = chess.Board(board.fen())
+            temp.push_san(str(move))
+
+            # get the current value of board
+            try:
+                curr_eval, f = getMove(temp, depth-1, False)
+            except:
+                curr_eval = getMove(temp, depth-1, False)
+            max = numpy.maximum(max, curr_eval)
+        return max
     else:
-        value = numpy.Infinity
-        bestEval = value
-        bestEvalMove = None
-        for i in node.child:
-            value, f = min(value, minimax(i, depth - 1, maximizingPlayer=True))
-            print("as")
-            if value < bestEval:
-                bestEval = value
-                bestEvalMove = f
-        return bestEval, bestEvalMove
+        minimum = numpy.Infinity
+        best_value_move = numpy.Infinity
+        move_list = list(board.legal_moves)
+        for i, move in enumerate(move_list):
+
+            # Create a separate board
+            temp = chess.Board(board.fen())
+            temp.push_san(str(move))
+
+            # get the current value of board
+            curr_eval = getMove(temp, depth-1, True)
+            minimum = numpy.minimum(minimum, curr_eval)
+
+            # if the new min is less than min_move set min_move to min and set best_min to the current best move
+            # because the new value of min is less than min_move it means it is the new best move.
+            if minimum < best_value_move:
+                best_value_move = minimum
+                best_move = move
+
+        return minimum, best_move
+
+# def boardEval(board, piece_values=piece_values):
+#     currEval = 0
+#     pieces = list(board.piece_map().values())
+#
+#     for i in pieces:
+#         currEval += piece_values[str(i)]
+#         print(currEval)
+#     return currEval
+#
+# def minimax(node, depth, maximizingPlayer = True):
+#
+#     # Base case
+#     print(type(depth))
+#     if depth == 0 or node.board.is_game_over():
+#         if maximizingPlayer:
+#             return boardEval(node.board), node.board
+#         else:
+#             return -1*boardEval(node.board), node.board
+#max
+#     # white
+#     if maximizingPlayer:
+#         value = -numpy.Infinity
+#         bestEval = value
+#         bestEvalMove = None
+#         for i in node.child:
+#             value, f = max(value, minimax(i, depth - 1, maximizingPlayer=True))
+#             print("as")
+#             if value > bestEval:
+#                 bestEval = value
+#                 bestEvalMove = f
+#         return bestEval, bestEvalMove
+#     # black
+#     else:
+#         value = numpy.Infinity
+#         bestEval = value
+#         bestEvalMove = None
+#         for i in node.child:
+#             value, f = min(value, minimax(i, depth - 1, maximizingPlayer=True))
+#             print("as")
+#             if value < bestEval:
+#                 bestEval = value
+#                 bestEvalMove = f
+#         return bestEval, bestEvalMove
 
 main()
